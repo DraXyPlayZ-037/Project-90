271ac7a1a62524f1b79dccfafc86f2074f3f1edd
DraXyPlayZ-037 <87331129+DraXyPlayZ-037@users.noreply.github.com>
2022-10-16 19:03:59 -0500

    Add files via upload

---
+++ gamepage.js
@@ -0,0 +1,17 @@
+p1n = localStorage.getItem("Player1");
+p2n = localStorage.getItem("Player2");
+
+document.getElementById("p1").innerHTML = p1n + " : ";
+document.getElementById("p2").innerHTML = p2n + " : ";
+
+
+p1s = 0;
+p2s = 0;
+
+
+document.getElementById("s1").innerHTML = p1s;
+document.getElementById("s2").innerHTML = p2s;
+
+function checkit() { //for upcoming project
+
+}
